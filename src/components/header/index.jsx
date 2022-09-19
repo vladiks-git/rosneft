add0a07f02c9d3026f59e8f238a461e711e0d327
AXBIT\vershov <vershov@clink.tech>
2022-09-19 11:55:25 +0400

    изменил хедер

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,30 +1,24 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-import { Button, IconButton, Stack, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-
-function Header() {
+export default function DenseAppBar() {
   return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      justifyContent="space-between"
-      sx={{ px: 4 }}
-    >
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton>
-      {/* <img src="../../assets/logo.jpg" alt="Logo" /> */}
-      <Typography variant="h5">Роснефть</Typography>
-      <Button variant="contained">fewfwefwefwefwefffe</Button>
-    </Stack>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+          Rosneft Health
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Header;
--- src/ui-kit/Button/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button as MUIButton } from "@mui/material";
-
-function Button() {
-  return <MUIButton />;
-}
-
-export default Button;
